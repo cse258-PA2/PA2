e5e6b8373cfa193cce642b5d52bf7c45057216d0
AlbertPi <wapi@ucsd.edu>
2019-12-02 00:44:57 -0800

    bag-of-words method added to construct feature from review text

--- PA2.py
+++ PA2.py
@@ -1,6 +1,7 @@
 import json
 import gzip
-import numpy
+import numpy as np
+import string
 from collections import defaultdict
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
@@ -18,6 +19,7 @@ import torch.optim as optim
 
 figs_path="/mnt/c/Users/AlbertPi/Desktop/"
 body_type_convertion_mod='onehot'  #'onehot' or 'integer'
+wordBag_size=1000
 
 #plot statistics of dataset
 def plot_dataset_statistics(dataset):
@@ -53,31 +55,74 @@ def plot_dataset_statistics(dataset):
         # plt.title(head+" statistics")
         plt.savefig(figs_path+head+"_statistics.png",dpi=300)
 
-## use to record features used in linear regression
-def feature(sample,mode):
-    if mode=='number':
-        dic={'fit':-1,'large':0,'small':1}
-        return [1,dic[sample['fit']]]
-    elif mode=='onehot':
-        dic={'fit':[1,0,0],'large':[0,1,0],'small':[0,0,1]}
-        return [1]+dic[sample['fit']]
+def bust_size_convertion(bust_size):
+    lower_size=int(bust_size[:2])
+    cup_size=bust_size[2:]
+    cup_size_dic={'aa':1,'a':2,'b':3,'c':4,'d':5,'d+':5.5,'dd':6,'ddd/e':7,'f':9,'g':11,'h':13,'i':15,'j':17}
+    return lower_size+cup_size_dic[cup_size]
+
+def body_type_convertion(body_type):
+    if body_type_convertion_mod=='onehot':
+        dic={'apple':'1000000','athletic':'0100000','full bust':'0010000','hourglass':'0001000','pear':'0000100','petite':'0000010','straight & narrow':'0000001'}
+    else:
+        dic={'apple':'1','athletic':'2','full bust':'3','hourglass':'4','pear':'5','petite':'6','straight & narrow':'7'}
+    return dic[body_type]
+
+##------------------------------------------------------------
+#                   Rating Prediction
+##------------------------------------------------------------
+
+def commonWords_generate(dataset,begin_index,end_index):
+    wordCount_dict=defaultdict(int)
+    punct = string.punctuation
+
+    for data in dataset:
+        review=data['review_text'].lower()
+        review=[char for char in review if (char not in punct)]
+        review=''.join(review)
+        data['review_text']=review
+        words=review.strip().split()
+        for word in words:
+            wordCount_dict[word]+=1
+    
+    wordANDcount=[(wordCount_dict[word],word) for word in wordCount_dict]
+    wordANDcount.sort(reverse=True)
+    commonWords=[sample[1] for sample in wordANDcount[begin_index:end_index]]
+    commonWordsID=dict(zip(commonWords,range(len(commonWords))))
+    return commonWordsID
+
+def freqVec_generate(review,commonWordsID):
+    words=review.strip().split()
+    vector=[0]*len(commonWordsID)
+    for word in words:
+        if word in commonWordsID:
+            vector[commonWordsID[word]]+=1
+    return vector
 
 def MSE(preds,labels):
     differences=[(x-y)**2 for (x,y) in zip(preds,labels)]
     MSE=sum(differences)/len(differences)
     return MSE
 
+def Naive_Average(labels_train,labels_valid):
+    pred=sum(labels_train)/len(labels_train)
+    preds_train=[pred]*len(labels_train)
+    preds_valid=[pred]*len(labels_valid)
 
-## predict rating using linear regression
-def Linear_Regression(dataset):
-        labels=[d['rating'] for d in dataset]
-        X=[feature(d,'onehot') for d in dataset]
-        X_train,X_valid,labels_train,labels_valid=train_test_split(X,labels,test_size=1/5,random_state=1)
+    MSE_train=MSE(preds_train,labels_train)
+    MSE_valid=MSE(preds_valid,labels_valid)
+    print("On train set, MSE = " + str(MSE_train)+", On valid set, MSE = " + str(MSE_valid))
+            
+    return MSE_valid
+
+def Linear_Regression(features_train,features_valid,labels_train,labels_valid):
         model=LinearRegression()
-        model.fit(X_train,labels_train)
+        features_train=np.array(features_train)
+        features_valid=np.array(features_valid)
+        model.fit(features_train[:,2:],labels_train)
         
-        preds_train=model.predict(X_train)
-        preds_valid=model.predict(X_valid)
+        preds_train=model.predict(features_train[:,2:])
+        preds_valid=model.predict(features_valid[:,2:])
         MSE_train=MSE(preds_train,labels_train)
         MSE_valid=MSE(preds_valid,labels_valid)
         print("On train set, MSE = " + str(MSE_train)+", On valid set, MSE = " + str(MSE_valid))
@@ -95,10 +140,10 @@ class LatentFactorModel(nn.Module):
         self.itemBias=nn.Parameter(torch.zeros(1,num_items))
         self.gamma_user=nn.Parameter(torch.zeros(num_users,K))
         self.gamma_item=nn.Parameter(torch.zeros(K,num_items))
-        self.fit_layer=nn.Linear(3,1)
+        self.fit_layer=nn.Linear(3+wordBag_size,1)
     
-    def forward(self,users,items,fit_info):
-        rating_preds=self.alpha+self.userBias[users,0]+self.itemBias[0,items]+(self.gamma_user[users,:]*torch.transpose(self.gamma_item[:,items],0,1)).sum()+self.fit_layer(fit_info).squeeze(1)
+    def forward(self,users,items,feature):
+        rating_preds=self.alpha+self.userBias[users,0]+self.itemBias[0,items]+(self.gamma_user[users,:]*torch.transpose(self.gamma_item[:,items],0,1)).sum()+self.fit_layer(feature).squeeze(1)
         # rating_preds=self.alpha+self.userBias[users,0]+self.itemBias[0,items]+self.fit_layer(fit_info).squeeze(1)
         return rating_preds
 
@@ -107,96 +152,90 @@ class DenseNet(nn.Module):
         super(DenseNet,self).__init__()
         self.gamma_users=nn.Parameter(torch.randn(num_users,K))
         self.gamma_items=nn.Parameter(torch.randn(num_items,K))
-        self.layer1=nn.Linear(2*K,num_Hidden)
-        self.layer1_activate=nn.ReLU()
-        self.layer2=nn.Linear(num_Hidden,1)
+        self.fullyConnect1=nn.Linear(2*K+3,num_Hidden)
+        self.activation1=nn.ReLU()
+        self.fullyConnect2=nn.Linear(num_Hidden,1)
     
-    def forward(self,users,items):
+    def forward(self,users,items,fit_info):
         users_vector=self.gamma_users[users,:]
         items_vector=self.gamma_items[items,:]
-        vector=torch.cat([users_vector,items_vector],1)
-        activated_vector=self.layer1_activate(self.layer1(vector))
-        # activated_vector=self.layer1(vector)
-        return self.layer2(activated_vector).squeeze(1)
+        vector=torch.cat([users_vector,items_vector,fit_info],1)
+        # vector=fit_info
+        activated_vector=self.activation1(self.fullyConnect1(vector))
+        # activated_vector=self.fullyConnect1(vector)
+        return self.fullyConnect2(activated_vector).squeeze(1)
 
-def rating_prediction_training(rating_dataset_train,rating_dataset_valid,rating_labels_train,rating_labels_valid,num_users,num_items):
+def rating_prediction_training(rating_features_train,rating_features_valid,rating_labels_train,rating_labels_valid,num_users,num_items,modelname):
     alpha=sum(rating_labels_train)/len(rating_labels_train)
+    rating_features_train=torch.tensor(rating_features_train,dtype=torch.long)
+    rating_features_valid=torch.tensor(rating_features_valid,dtype=torch.long)
+    rating_labels_train=torch.tensor(rating_labels_train,dtype=torch.float32)
+    rating_labels_valid=torch.tensor(rating_labels_valid,dtype=torch.float32)
 
-    model=LatentFactorModel(num_users,num_items,5,alpha)
-    # model=DenseNet(num_users,num_items,4,25)
+    if modelname=='LatentFactorModel':
+        model=LatentFactorModel(num_users,num_items,5,alpha)
+        learning_rate=5e-1
+        optimizer=optim.SGD(model.parameters(),lr=learning_rate,momentum=0.8,weight_decay=5e-4)
+    elif modelname=='DenseNet':
+        model=DenseNet(num_users,num_items,5,30)
+        learning_rate=1e-2
+        optimizer=optim.SGD(model.parameters(),lr=learning_rate,momentum=0.8,weight_decay=5e-5)
+        # optimizer=optim.Adam(model.parameters(),lr=learning_rate,weight_decay=5e-3)
+
     loss_func=MSE_loss
-    learning_rate=5e-2
-    optimizer=optim.SGD(model.parameters(),lr=learning_rate,momentum=0.8,weight_decay=5e-4)
-    # optimizer=optim.Adam(model.parameters(),lr=learning_rate,weight_decay=5e-3)
 
     train_epochs=20000
     for epoch in range(train_epochs):
 
         optimizer.zero_grad()
-        rating_preds_train=model(rating_dataset_train[:,0],rating_dataset_train[:,1],rating_dataset_train[:,2:].float())
+        rating_preds_train=model(rating_features_train[:,0],rating_features_train[:,1],rating_features_train[:,2:].float())
         loss_train=loss_func(rating_preds_train,rating_labels_train)
         loss_train.backward()
 
         optimizer.step()
 
-        rating_preds_valid=model(rating_dataset_valid[:,0],rating_dataset_valid[:,1],rating_dataset_valid[:,2:].float())
+        rating_preds_valid=model(rating_features_valid[:,0],rating_features_valid[:,1],rating_features_valid[:,2:].float())
         loss_valid=loss_func(rating_preds_valid,rating_labels_valid)
         if epoch<5 or epoch%100==0:
             print("Epoch: %d, train loss is: %f, validation loss is: %f" %(epoch,float(loss_train),float(loss_valid)))
     return loss_valid
 
 def rating_prediction(dataset,modelname):
-    user_set=set()
-    item_set=set()
+
+    dataset_train,dataset_valid=train_test_split(dataset,test_size=1/5,random_state=1)
+    commonWordsID=commonWords_generate(dataset_train,0,0+wordBag_size)
+
+    user_set,item_set=set(),set()
     for data in dataset:
         user_set.add(data['user_id'])
         item_set.add(data['item_id'])
-    users=list(user_set)
-    items=list(item_set)
-    userIndex_dic={}
-    itemIndex_dic={}
-    cnt=0
-    for user in users:
-        userIndex_dic[user]=cnt
-        cnt+=1
-    cnt=0
-    for item in items:
-        itemIndex_dic[item]=cnt
-        cnt+=1
+    users,items=list(user_set),list(item_set)
+    userIndex_dic,itemIndex_dic={},{}
+    userIndex_dic=dict(zip(users,range(len(users))))
+    itemIndex_dic=dict(zip(items,range(len(items))))
 
-    rating_dataset=[]
-    rating_labels=[]
+    rating_features,rating_labels,wordFeq_vectors=[],[],[]
     fit_dic={'fit':[1,0,0],'large':[0,1,0],'small':[0,0,1]}
     for data in dataset:
-        rating_dataset.append([userIndex_dic[data['user_id']],itemIndex_dic[data['item_id']]]+fit_dic[data['fit']])
+        rating_features.append([userIndex_dic[data['user_id']],itemIndex_dic[data['item_id']]]+fit_dic[data['fit']]+freqVec_generate(data['review_text'],commonWordsID))
         rating_labels.append(data['rating'])
-        
     
-    rating_dataset_train,rating_dataset_valid,rating_labels_train,rating_labels_valid=train_test_split(rating_dataset,rating_labels,test_size=1/5)
-    rating_dataset_train=torch.tensor(rating_dataset_train,dtype=torch.long)
-    rating_dataset_valid=torch.tensor(rating_dataset_valid,dtype=torch.long)
-    rating_labels_train=torch.tensor(rating_labels_train,dtype=torch.float32)
-    rating_labels_valid=torch.tensor(rating_labels_valid,dtype=torch.float32)
+    rating_features_train,rating_features_valid,rating_labels_train,rating_labels_valid=train_test_split(rating_features,rating_labels,test_size=1/5,random_state=1)
     
-    if modelname=="LinearRegression":
-        MSE=Linear_Regression(dataset)
+    if modelname=='NaiveAverage':
+        MSE=Naive_Average(rating_labels_train,rating_labels_valid)
+    elif modelname=="LinearRegression":
+        MSE=Linear_Regression(rating_features_train,rating_features_valid,rating_labels_train,rating_labels_valid)
     elif modelname=='LatentFactorModel':
-        MSE=rating_prediction_training(rating_dataset_train,rating_dataset_valid,rating_labels_train,rating_labels_valid,len(users),len(items))
+        MSE=rating_prediction_training(rating_features_train,rating_features_valid,rating_labels_train,rating_labels_valid,len(users),len(items),'LatentFactorModel')
+    elif modelname=='DenseNet':
+        MSE=rating_prediction_training(rating_features_train,rating_features_valid,rating_labels_train,rating_labels_valid,len(users),len(items),'DenseNet')
     
     print("MSE of "+modelname+" on validation set is: %f" %MSE)
 
-def bust_size_convertion(bust_size):
-    lower_size=int(bust_size[:2])
-    cup_size=bust_size[2:]
-    cup_size_dic={'aa':1,'a':2,'b':3,'c':4,'d':5,'d+':5.5,'dd':6,'ddd/e':7,'f':9,'g':11,'h':13,'i':15,'j':17}
-    return lower_size+cup_size_dic[cup_size]
-
-def body_type_convertion(body_type):
-    if body_type_convertion_mod=='onehot':
-        dic={'apple':'1000000','athletic':'0100000','full bust':'0010000','hourglass':'0001000','pear':'0000100','petite':'0000010','straight & narrow':'0000001'}
-    else:
-        dic={'apple':'1','athletic':'2','full bust':'3','hourglass':'4','pear':'5','petite':'6','straight & narrow':'7'}
-    return dic[body_type]
+##------------------------------------------------------------
+#                   Fit Prediction
+##------------------------------------------------------------
 
 def classification_statics(preds,labels):
     cm=metrics.confusion_matrix(labels,preds)
@@ -218,7 +257,7 @@ def Logistic_Regression(features_train,features_valid,labels_train):
     return preds_train,preds_valid
 
 def SVM(features_train,features_valid,labels_train):
-    model=SVC(kernel='linear',C=0.1,random_state=1,gamma='auto',max_iter=2000, class_weight={'fit':5, 'small':8, 'large':9})
+    model=SVC(kernel='sigmoid',C=0.1,random_state=1,gamma='auto',max_iter=2000)
     model.fit(features_train,labels_train)
     preds_train=model.predict(features_train)
     preds_valid=model.predict(features_valid)
@@ -247,7 +286,7 @@ def fit_prediction(dataset,modelname):
         features.append(feature)
         labels.append(data['fit'])
 
-    features_train,features_valid,labels_train,labels_valid=train_test_split(features,labels,test_size=0.2)
+    features_train,features_valid,labels_train,labels_valid=train_test_split(features,labels,test_size=1/5,random_state=1)
 
     if modelname=="LogisticRegression":
         preds_train,preds_valid=Logistic_Regression(features_train,features_valid,labels_train)
@@ -271,6 +310,16 @@ def fit_prediction(dataset,modelname):
     print('-----------------------------------------')
     classification_statics(preds_valid,labels_valid)
 
+def category_size_distribution(dataset):
+    size_dic=defaultdict(set)
+    count_dic=defaultdict(int)
+    for data in dataset:
+        size_dic[data['category']].add(data['size'])
+        count_dic[data['category']]+=1
+    
+    for key in size_dic:
+        print(key+': '+str(count_dic[key])+str(size_dic[key]))
+
 
 #Possible tasks: 1. predict whether fit  2. predict rating 3. whether A would rent B
 if __name__ == "__main__":
@@ -281,10 +330,6 @@ if __name__ == "__main__":
             data=json.loads(line)
             if data['rating']==None or len(data)!=15:  #If it doesn't have all attributes or rating is None, abort thus sample  
                 continue        
-            if 'perfect' in data['review_summary'] or 'glamourous' in data['review_summary'] or 'love' in data['review_summary'] or 'cute' in data['review_summary'] or 'compliments' in data['review_summary'] or 'confident' in data['review_summary'] or 'awesome' in data['review_summary'] or 'comfortable' in data['review_summary'] or 'fashion' in data['review_summary'] or 'trendy' in data['review_summary'] or 'great' in data['review_summary'] or 'best' in data['review_summary'] or 'gorgeous' in data['review_summary'] or 'beautiful' in data['review_summary'] or 'recommend' in data['review_summary'] or 'fit' in data['review_summary'] or 'fun' in data['review_summary']:
-                data['good_review']=1
-            else:
-                data['good_review']=0
             data['age']=int(data['age'])
             data['rating']=int(data['rating'])
             data['weight']=int(data['weight'][:-3])
@@ -294,22 +339,24 @@ if __name__ == "__main__":
             data['body type']=body_type_convertion(data['body type'])
             dataset.append(data)
 
-
-
     print('Read dataset complete')
+
+    # category_size_distribution(dataset)
 
 #-------------------------plot statistics of dataset-----------------------------
     #plot_dataset_statistics(dataset)
 
 #-------------------------rating prediction--------------------------------------
+    rating_prediction(dataset,'NaiveAverage')
     # rating_prediction(dataset,'LinearRegression')
     # rating_prediction(dataset,'LatentFactorModel')
+    # rating_prediction(dataset,'DenseNet')
     
 #-------------------------fit prediction-----------------------------------------
     # print('-----------------------------------------')
     # print('Using LR')
     # print('-----------------------------------------')
-    fit_prediction(dataset,"LogisticRegression")
+    # fit_prediction(dataset,"LogisticRegression")
 
     # print('-----------------------------------------')
     # print('Using SVM')
